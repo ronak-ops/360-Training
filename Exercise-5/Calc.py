3348aabcb59c992dda6c123b1807f2558567d0cb
Ronak Chandgadhia <55931910+ronak-ops@users.noreply.github.com>
2022-07-27 13:17:52 -0400

    Updated Calc file

--- Exercise-5/Calc.py
+++ Exercise-5/Calc.py
@@ -1,11 +1,11 @@
 def add(a,b):
-    pass
+    return(a+b)
 
 def subtract(a,b):
-    pass
+    return(a-b)
 
 def multiply(a,b):
-    pass
+    return(a*b)
 
 def divide(a,b):
-    pass
+    return(a/b)
